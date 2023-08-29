1339f332065ded15349797d04fcd2bde4d9f04ac
susmie86 <susmie600@gmail.com>
2023-08-29 21:03:14 +0545

    Added Cart with features like Add product remove product Increase quantity of product

---
+++ src/assets/Icons/MinusIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MinusIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M20 12H4" stroke="black" strokeWidth="1.5" strokeLinecap="round" />
+        </svg>
+    )
+}
+
+export default MinusIcon
---
+++ src/components/Cart/CartCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CartCard(props) {
+  return <div className="card">{props.children}</div>;
+}
+
+export default CartCard;
